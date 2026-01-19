6e9d118b04ed67a50836893e526cf65fe4c85297
IT-imataka <takashi6128_gakky@outlook.com>
2026-01-20 02:23:25 +0900

    feat: implement duplicate reservation check

--- backend/src/controllers/reservationControllers.ts
+++ backend/src/controllers/reservationControllers.ts
@@ -4,20 +4,30 @@ import { Request, Response } from "express";
 import * as reservationService from "../services/reservationService";
 
 export const createReservation = (req: Request, res: Response) => {
-  // クライアントからのjsonデータを受け取る
-  // index.tsでexpress.jsonのミドルの設定をしているのでbodyにある
-  // 分割代入でぶっこむ
-  const { useId, userId, startTime, endTime } = req.body;
+  // Serviceで返ってきたエラーをここでハンドリングしてステータスコードを返す
+  try {
+    // クライアントからのjsonデータを受け取る
+    // index.tsでexpress.jsonのミドルの設定をしているのでbodyにある
+    // 分割代入でぶっこむ
+    const { useId, userId, startTime, endTime } = req.body;
 
-  // Serviceのビジネスロジックに渡して処理
-  // 変数は同じ変数名でもう一度宣言する
-  const newreservation = reservationService.createReservation({
-    useId,
-    userId,
-    startTime,
-    endTime,
-  });
+    // Serviceのビジネスロジックに渡して処理
+    // 変数は同じ変数名でもう一度宣言する
+    const newreservation = reservationService.createReservation({
+      useId,
+      userId,
+      startTime,
+      endTime,
+    });
 
-  // 結果を返す
-  res.status(201).json(newreservation);
+    res.status(201).json(newreservation);
+  } catch (error) {
+    console.error(error);
+    // ここでクライアント側に補足したエラーをステータスコードとして返す
+    if (error instanceof Error) {
+      res.status(400).json({ message: error.message });
+    } else {
+      res.status(500).json({ message: "予期せぬエラーが発生しました" });
+    }
+  }
 };
