6456ec5fd931f393295422435a1e4490cb7c2639
IT-imataka <takashi6128_gakky@outlook.com>
2026-02-11 17:07:00 +0900

    feat: implement 'reservables' hooks and API

--- frontend/src/api/reservableApi.ts
+++ frontend/src/api/reservableApi.ts
@@ -5,6 +5,7 @@ import type { Reservable } from "./reservationApi";
 const API_BASE_PORT = 3000;
 const API_BASE_URL = `:http://localhost:${API_BASE_PORT}`;
 
+// 取得処理
 export const fetchgReservables = async (): Promise<Reservable[]> => {
   const res = await fetch(`${API_BASE_URL}/reservables`);
   // okプロパティで存在確認
@@ -13,3 +14,30 @@ export const fetchgReservables = async (): Promise<Reservable[]> => {
   // エラーハンドリングはhooksでやるエラーハンドリングはhooksでやるエラーハンドリングはhooksでやるエラーハンドリングはhooksでやる
   return res.json();
 };
+
+// 登録処理 idはサーバー側で決めるから不要 reservationsの時は特定のものを参照していたから必要だった
+export const regReservables = async (
+  name: string,
+  type: "ROOM" | "EQUIPMENT",
+): Promise<Reservable[]> => {
+  const res = await fetch(`${API_BASE_URL}/reservables`, {
+    // どの渡し方か
+    method: "POST",
+    // 何で渡すか
+    headers: {
+      "content-type": "application/json",
+    },
+    // どうやって、それを何で渡すか
+    body: JSON.stringify({
+      name: name,
+      type: type,
+      isActive: true,
+    }),
+  });
+  if (!res.ok) throw new Error("登録に失敗しました");
+  return res.json();
+};
+
+// 削除処理
+
+// 更新処理
--- frontend/src/hooks/useReservables.ts
+++ frontend/src/hooks/useReservables.ts
@@ -1,10 +1,15 @@
 import { useEffect, useState } from "react";
 import type { Reservable } from "../api/reservationApi";
 import { fetchReservables } from "../api/reservationApi";
+import { regReservables } from "../api/reservableApi";
 
 // 名前自由関数で
 export const useReservables = () => {
+  // 情報を持っておく箱
   const [reservables, setReserbables] = useState<Reservable[]>([]);
+  const [name, setName] = useState<string>("");
+  const [type, setType] = useState<"ROOM" | "EQUIPMENT">("ROOM");
+  const [isActive, setIsACtive] = useState<boolean>(true);
 
   // 画面描画時にレンダリング
   // フェッチしたデータで状態の更新を行う
@@ -19,5 +24,14 @@ export const useReservables = () => {
       loadData();
     };
   }, []);
+
+  // hooksの中は処理だけさせる
+  const handleRegister = async () => {
+    try {
+      await regReservables(name, type);
+    } catch (error) {
+      console.error("登録に失敗しました", error);
+    }
+  };
   return { reservables };
 };
